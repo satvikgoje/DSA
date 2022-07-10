65b95ae06dbee7be111086661f886cc01b768b7d
satvikgoje <satvikgoje2410@gmail.com>
2022-07-11 01:06:20 +0530

    Day2-10th July

--- Codeforces/football.cpp
+++ Codeforces/football.cpp
@@ -1,28 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-string s;
-cin>>s;
-int k=7;
-int n=s.length();
-for (int i = 0; i <=n-k; i++)
+int main()
 {
-    int count{0};
-    for (int j = i; j <= i+k-1; j++)
+    string s;
+    cin >> s;
+    int k = 7;
+    int n = s.length();
+    // for (int i = 0; i <=n-k; i++)
+    // {
+    //     int count{0};
+    //     for (int j = i; j <= i+k-1; j++)
+    //     {
+    //        if(s[i]==s[j])
+    //        {
+    //         count++;
+    //        }
+    //     }
+    //     if(count==k){
+    //         cout<<"YES";
+    //         return 0;
+    //     }
+    // }
+
+    // cout<<"NO";
+    int max_global{1};
+    int count{1};
+    for (int i = 0; i <= n - 2; i++)
     {
-       if(s[i]==s[j])
-       {
-        count++;
-       }
+        if (s[i] == s[i + 1])
+        {
+            count++;
+            max_global = max(max_global, count);
+        }
+        else
+        {
+            count = 1;
+        }
     }
-    if(count==k){
+    if(max_global>=k){
         cout<<"YES";
-        return 0;
     }
-}
-
-cout<<"NO";
-
-
+    else
+       cout<<"NO";
 }
---
+++ learning code and extras/repetition.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int max_global = {1};
+    int n = s.length();
+    int count{1};
+
+    for (int i = 0; i <= n - 2; i++)
+    {
+        if (s[i] == s[i + 1])
+        {
+            count++;
+            max_global = max(max_global, count);
+        }
+        else
+        {
+            count = 1;
+        }
+    }
+    cout << max_global;
+
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //     int count={0};
+    //     for (int j = i; j < n; j++)
+    //     {
+    //         if(s[i]==s[j]){
+    //             count++;
+    //             max_global=max(max_global,count);
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+
+    //  }
+    // cout << max_global;
+}
